a131a1574b0ee7c44c7bd8b5cf05ff724be395dc
JAY <elinmedia10@gmail.com>
2018-03-27 04:04:59 +0900

    Initial commit

---
+++ src/Movie.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
+import './Movie.css';
+
+// class Movie extends Component {
+//
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired,
+//         genres: PropTypes.array.isRequiored,
+//         synopsis: PropTypes.string.isRequired
+//     }
+//
+//     render() {
+//         return (
+//             <div className="Movie">
+//                 <div className="Movie__Columns">
+//                 <MoivePoster poster={this.props.poster} title={this.props.title}/>
+//                 </div>
+//                 <div className="Movie__Columns">
+//                     <h1>{this.props.title}</h1>
+//                     <div className="Movie_Genres">
+//
+//                     </div>
+//                 </div>
+//             </div>
+//
+//         )
+//     }
+// }
+//
+//
+// class MoivePoster extends Component {
+//
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired
+//     }
+//     render() {
+//         return(
+//             <img src={this.props.poster} alt={this.props.title}/>
+//         )
+//     }
+// }
+
+function Movie({title, poster, genres, synopsis}) {
+    return (
+        <div className="Movie">
+            <div className="Movie__Column">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Column">
+                <h1>{title}</h1>
+                <div className="Movie_Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+                <div className="Movie__Synopsis">
+                    <LinesEllipsis
+                        text={synopsis}
+                        maxLine='3'
+                        ellipsis=' ...'
+                        trimRight
+                        basedOn='letters'
+                        />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function MoviePoster({poster, alt}) {
+    return (
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
+        )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre} </span>
+    )
+}
+Movie.propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired,
+        genres: PropTypes.array.isRequired,
+        synopsis: PropTypes.string.isRequired
+}
+
+MoviePoster.prototypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+
+
+export default Movie
